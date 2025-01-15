343d07f305e01c482e5c8f819bde2f019e84ea3e
Kannapi <112032889+itsKannapi@users.noreply.github.com>
2025-01-15 12:41:44 -0600

    Second Day Coding

---
+++ CIS_144_Week1/src/myfirstProgram/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package myfirstProgram;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        int Hours = 40;
+        double PayRate = 16.5;
+
+        double Payment = Hours * PayRate;
+
+        System.out.println(" Your Payment is => " + Payment);
+    }
+}
