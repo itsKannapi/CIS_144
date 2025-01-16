8c08405a130dd3b4df73a08acee88b9b9aba1a90
Kannapi <112032889+itsKannapi@users.noreply.github.com>
2025-01-16 13:59:31 -0600

    idk what i did

--- CIS_144_Week1/src/myfirstProgram/MyFirstProgram.java
+++ CIS_144_Week1/src/myfirstProgram/MyFirstProgram.java
@@ -1,13 +1,12 @@
 package myfirstProgram;
 
 public class MyFirstProgram {
-
     public static void main(String[] args) {
         int Hours = 40;
         double PayRate = 16.5;
-
         double Payment = Hours * PayRate;
 
         System.out.println(" Your Payment is => " + Payment);
+        
     }
 }
